4d23456276b11bbeb8bc435975bac2277a4ab219
Peter Lamson <ptrlmsn@gmail.com>
2022-02-28 16:50:33 -0500

    testing

--- meadowlark.js
+++ meadowlark.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const expressHandlebars = require('express-handlebars')
-const fortune = require('./lib/fortune.js')
+const handlers = require('./lib/handlers')
 
 const app = express()
 
@@ -14,23 +14,16 @@ app.use(express.static(__dirname + '/public'))
 
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => res.render('home'))
+app.get('/', handlers.home)
 
-app.get('/about', (req, res) => { 
-    res.render('about', {fortune: fortune.getFortune})}) 
+app.get('/about', handlers.about)
 
 // custom 404 page
-app.use((req, res) => {
-  res.status(404)
-  res.render('404')
-})
+app.use(handlers.notFound)
+
 
 // custom 500 page
-app.use((err, req, res, next) => {
-  console.error(err.message)
-  res.status(500)
-  res.render('500')
-})
+app.use(handlers.serverError)
 
 app.listen(port, () => console.log(
   `Express started on http://localhost:${port}; ` +
